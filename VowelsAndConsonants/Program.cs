3088403bddea4b7b1ea6f5c886b41d6173a3eb0c
Kamran Z <kamraneze@code.edu.az>
2023-08-28 11:31:39 +0400

    Consonants and vowels

---
+++ VowelsAndConsonants/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace VowelsAndConsonants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "KamrManAEMM";
+            string allVowels = "AEIOUaeiou";
+            int vowels = 0;
+            int consonants = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (allVowels.Contains(word[i])) vowels++;
+                else if (char.IsLetter(word[i]) && !allVowels.Contains(word[i])) consonants++;
+            }
+
+            Console.WriteLine("Vowels: " + vowels);
+            Console.WriteLine("Consonants :" + consonants);
+
+
+        }
+    }
+}
